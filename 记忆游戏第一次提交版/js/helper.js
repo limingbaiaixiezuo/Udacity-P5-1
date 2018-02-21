ea795d65f393f9f0586e9f8ce6c72bb880dab626
lichunming <limingbaiaixiezuo@gmail.com>
2018-02-21 18:20:51 +0800

    First commit
    good luck

---
+++ 记忆游戏第一次提交版/js/helper.js
@@ -0,0 +1,18 @@
+
+
+var diamond = '<li class="card "><i class="fa fa-diamond"></i></li>';
+var diamond2 = '<li class="card "><i class="fa fa-diamond"></i></li>';
+var paper = '<li class="card "><i class="fa fa-paper-plane-o"></i></li>';
+var paper2 = '<li class="card "><i class="fa fa-paper-plane-o"></i></li>';
+var anchor = '<li class="card "><i class="fa fa-anchor"></i></li>';
+var anchor2 = '<li class="card "><i class="fa fa-anchor"></i></li>';
+var bolt = '<li class="card "><i class="fa fa-bolt"></i></li>';
+var bolt2 = '<li class="card "><i class="fa fa-bolt"></i></li>';
+var cube = '<li class="card "><i class="fa fa-cube"></i></li>';
+var cube2 = '<li class="card "><i class="fa fa-cube"></i></li>';
+var leaf = '<li class="card "><i class="fa fa-leaf"></i></li>';
+var leaf2 = '<li class="card "><i class="fa fa-leaf"></i></li>';
+var bicycle = '<li class="card "><i class="fa fa-bicycle"></i></li>';
+var bicycle2 = '<li class="card "><i class="fa fa-bicycle"></i></li>';
+var bomb = '<li class="card "><i class="fa fa-bomb"></i></li>';
+var bomb2 = '<li class="card "><i class="fa fa-bomb"></i></li>';
